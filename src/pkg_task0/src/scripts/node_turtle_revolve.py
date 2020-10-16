cb1a67544bb4be76d5a5a5784ea961d9189f5e1c
himanshunandan292 <himanshu.kvpy1@gmail.com>
2020-10-17 01:02:23 +0530

    added launch file , bag file and modified node

--- src/pkg_task0/src/scripts/node_turtle_revolve.py
+++ src/pkg_task0/src/scripts/node_turtle_revolve.py
@@ -1,27 +1,35 @@
 #!/usr/bin/env python
 import rospy
 from geometry_msgs.msg import Twist
-from geometry_msgs.msg import Pose
+from turtlesim.msg import Pose
+from math import pi
 def move_circle():
     pub = rospy.Publisher('turtle1/cmd_vel', Twist , queue_size=10)
     rospy.init_node('move_circle' , anonymous=True)
     rate = rospy.Rate(10)
-    while not rospy.is_shutdown():
-        arg = Twist()
-        pose = Pose()
-        w = 1.0
-        r = 2.0
-        theta = w/r
-        arg.linear.x = w
-        arg.linear.y = 0
-        arg.linear.z = 0
-        arg.angular.x = 0
-        arg.angular.y = 0
-        arg.angular.z = theta
+    theta = 0
+    arg = Twist()
+    pose = Pose()
+    w = 1.0
+    r = 2.0
+    theta = w/r
+    arg.linear.x = w
+    arg.linear.y = 0
+    arg.linear.z = 0
+    arg.angular.x = 0
+    arg.angular.y = 0
+    arg.angular.z = theta
+    def pose_callback(msg):
+        print(msg.theta)
+    sub = rospy.Subscriber('turtle1/pose' , Pose , pose_callback)
+    pub.publish(arg)
+    rospy.loginfo(pose)
+    time = rospy.Time.now()
+    rate = rospy.Rate(20)
+    while rospy.Time.now() < time + rospy.Duration.from_sec(12):
         pub.publish(arg)
-        print(pose.orientation.w)
+        rospy.loginfo(pose_callback)
         rate.sleep()
-        
 if __name__ == '__main__':
     try:
         move_circle()
